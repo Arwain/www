960768f2543e615fa6b96c532194578dbe75034f
Brandon G <Bgaetaniello@gmail.com>
2017-11-30 22:10:04 -0800

    a

--- inc.dbc.php
+++ inc.dbc.php
@@ -1,11 +1,8 @@
 <?php
 
 	function get_connection() {
-		$userid   = 'ak99'; //Change this to yours
-		$password = 'ekfuxvaw'; //Change this to yours
 		$userid   = 'bmsg'; //Change this to yours
 		$host     = 'cssgate.insttech.washington.edu';
-		$dbname   = 'ak99'; //Change this to yours
 		$dbname   = 'bmsg'; //Change this to yours
 		
 		$dsn = 'mysql:host='.$host. ';dbname='.$dbname;
