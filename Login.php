351cbc4f32ec680b5ac6484e48a553cc66265ff6
Mahad <mahadf@uw.edu>
2017-12-01 17:04:59 -0800

    slight changes

--- Login.php
+++ Login.php
@@ -3,81 +3,84 @@
 
 if (isset($_POST['login'])) // HANDLE THE FORM
 {
-   
-  // CHECK TO ENSURE INPUTS ARE VALID
-  if (strlen($_POST['username']) == 0 || strlen($_POST['pwd']) == 0)
-  {
-    $errors = '<p class="alert-danger">Please enter both a username and a password</p>';
-  } else {
-    // CHECK USER IN DATABASE, THIS IS SUSEPTABLE TO SQL INJECTION AND FAILURE DUE TO QUOTES
-    if(!isset($db))
+
+    // CHECK TO ENSURE INPUTS ARE VALID
+    if (strlen($_POST['username']) == 0 || strlen($_POST['pwd']) == 0)
     {
-      require ('inc.dbc.php');
-      $db = get_connection();
-    }
-    
-    // QUERY TO VALIDATE USER
-    $q = "SELECT userid, username, pwd, role 
+        $errors = '<p class="alert-danger">Please enter both a username and a password</p>';
+    } else {
+        // CHECK USER IN DATABASE, THIS IS SUSEPTABLE TO SQL INJECTION AND FAILURE DUE TO QUOTES
+        if(!isset($db))
+        {
+            require ('inc.dbc.php');
+            $db = get_connection();
+        }
+
+        // QUERY TO VALIDATE USER
+        $q = "SELECT userid, username, pwd, role 
            FROM User 
           WHERE username = '".$_POST['username']."' 
             AND pwd = '"     .md5($_POST['pwd'])."'";
 
-    // EXECUTE THE QUERY
-    $r = $db->query($q);
-    $rr = $r->fetchAll();
-    if(count($rr) == 1) // ASSUMES UNIQUENESS OF USERID SET IN DATABASE
-    {
-      //DATA RETURNED SETUP MY SESSION
-      @session_start();
-      $_SESSION['userid']   = $rr[0]['userid'];
-      $_SESSION['role']     = $rr[0]['role'];
-      
-      // REDIRECT TO THE CORRECT PORTAL
-      $location = ($_SESSION['role'] == 'teacher') ? "ManageCourses.php" : "Profile.php";
-      header("Location: " . $location);
-    } else { // THROW ERROR
-      $errors .= $errors . '<p class="alert-danger">Invalid Username/Password</p>';
+        // EXECUTE THE QUERY
+        $r = $db->query($q);
+        $rr = $r->fetchAll();
+        if(count($rr) == 1) // ASSUMES UNIQUENESS OF USERID SET IN DATABASE
+        {
+            //DATA RETURNED SETUP MY SESSION
+            @session_start();
+            $_SESSION['userid']   = $rr[0]['userid'];
+            $_SESSION['role']     = $rr[0]['role'];
+
+            // REDIRECT TO THE CORRECT PORTAL
+            $location = ($_SESSION['role'] == 'teacher') ? "ManageCart.php" : "Profile.php";
+            header("Location: " . $location);
+        } else { // THROW ERROR
+            $errors .= $errors . '<p class="alert-danger">Invalid Username/Password</p>';
+        }
     }
-  }
 }
 ?>
 
 <!DOCTYPE html>
 <html lang="en">
-  <head>
+<head>
     <meta charset="utf-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <title>Mario Cart Sign In</title>
     <link href="./css/bootstrap.min.css" rel="stylesheet">
     <link href="./css/signin.css" rel="stylesheet">
-      <style>body {
-              background-image: url("images/mclogo.jpg");
 
-          } </style>
+    <style>body {
+            background-image: url("images/mario.gif");
+
+        } </style>
     <script src="https://oss.maxcdn.com/html5shiv/3.7.3/html5shiv.min.js"></script>
     <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
-  </head>
+</head>
 
-  <body>
+<body>
+
 <div class="container">
-	<!-- BEGIN CONTENT -->
-	<h2 class="text-center">Welcome to Mario Cart!</h2>
-	
-	<form class="form-signin" role="form" method="POST" action="<?php echo $_SERVER['PHP_SELF']; ?>">
-		<h4 class="form-signin-heading text-center">Please enter your username and password</h4>
-		<input type="text" class="form-control" placeholder="Username" required name = "username">
-		<input type="password" class="form-control" placeholder="Password" required name="pwd"/>
-		<button class="btn btn-lg btn-primary btn-block" type="submit" name="login">
-			Enter
-		</button>
-	</form>
+    <!-- BEGIN CONTENT -->
+    <h2 class="text-center">Welcome to Mario Cart!</h2>
+
+    <form class="form-signin" role="form" method="POST" action="<?php echo $_SERVER['PHP_SELF']; ?>">
+        <h4 class="form-signin-heading text-center">Please enter your username and password</h4>
+        <input type="text" class="form-control" placeholder="Username" required name = "username">
+        <input type="password" class="form-control" placeholder="Password" required name="pwd"/>
+        <button class="btn btn-lg btn-primary btn-block" type="submit" name="login">
+            Enter
+        </button>
+    </form>
+    <audio  autoplay hidden ><source src = "sounds/startheme.mp3" type = "audio/mp3"
 </div>
-  
-    
 
 
-    <!-- IE10 viewport hack for Surface/desktop Windows 8 bug -->
-    <script src="../../assets/js/ie10-viewport-bug-workaround.js"></script>
-  </body>
+
+
+<!-- IE10 viewport hack for Surface/desktop Windows 8 bug -->
+<script src="../../assets/js/ie10-viewport-bug-workaround.js"></script>
+</body>
 </html>
